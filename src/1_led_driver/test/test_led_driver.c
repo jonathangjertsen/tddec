7d4b6da6137447182c674b9d44b38339460e7747
Jonathan Reichelt Gjertsen <jonathan.gjertsen@disruptive-technologies.com>
2020-06-11 19:49:10 +0200

    Move LED_Create into setUp

--- src/1_led_driver/test/test_led_driver.c
+++ src/1_led_driver/test/test_led_driver.c
@@ -2,25 +2,28 @@
 #include <unity.h>
 #include <stdint.h>
 
+static uint16_t virtualLeds;
+
+void setUp(void)
+{
+    LED_Create(&virtualLeds);
+}
+
 void test_offAfterCreate(void)
 {
-    uint16_t virtualLeds = 0xffff;
+    virtualLeds = 0xffff;
     LED_Create(&virtualLeds);
     TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
 }
 
 void test_turnOn(void)
 {
-    uint16_t virtualLeds;
-    LED_Create(&virtualLeds);
     LED_On(1);
     TEST_ASSERT_EQUAL_HEX16(0x0001, virtualLeds);
 }
 
 void test_turnOff(void)
 {
-    uint16_t virtualLeds;
-    LED_Create(&virtualLeds);
     LED_On(1);
     LED_Off(1);
     TEST_ASSERT_EQUAL_HEX16(0x0000, virtualLeds);
